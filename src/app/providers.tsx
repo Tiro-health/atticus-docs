ffd70b19ca97ad93c8c1590cd6ecd1263a1016ec
Axel Vanraes <axel.vanraes@tiro.health>
2025-08-22 11:42:00 +0200

    setup posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,10 @@
 
 import { useEffect } from 'react'
 import { ThemeProvider, useTheme } from 'next-themes'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { usePostHog } from 'posthog-js/react'
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 function ThemeWatcher() {
   let { resolvedTheme, setTheme } = useTheme()
@@ -27,11 +31,26 @@ function ThemeWatcher() {
   return null
 }
 
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      defaults: '2025-05-24',
+    })
+  }, [])
+
+  return <PHProvider client={posthog}>{children}</PHProvider>
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" disableTransitionOnChange>
-      <ThemeWatcher />
-      {children}
-    </ThemeProvider>
+    <PostHogProvider>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <ThemeWatcher />
+        {children}
+      </ThemeProvider>
+    </PostHogProvider>
   )
 }
